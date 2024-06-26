375b5fd9e35002741d1de72406d1b62a2bede1ac
sutaC <catus.pl@gmail.com>
2024-06-26 16:58:43 +0200

    Revert "Reading args"
    This reverts commit 7660b2c0b792bd8277e3ba1c9c0def03a55ffd08.

--- main.js
+++ main.js
@@ -14,10 +14,9 @@ try {
 } catch (err) {
     throw new Error(`Could not access given path file:\n${err}`);
 }
-const file = await fs.open(path);
-const fileReader = readline.createInterface({ input: file.createReadStream() });
+const code = (await fs.readFile(path)).toString();
 
-// Setup I/O
+// Setup std I/O
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -30,64 +29,65 @@ const eBrackets = /** @type {number[]} */ [];
 let dp = 0;
 
 // Interpreter run
-fileReader.on("line", async (line) => {
-    for (let i = 0; i < line.length; i++) {
-        switch (line[i]) {
-            case "+":
-                data[dp]++;
-                break;
-            case "-":
-                data[dp]--;
-                break;
-            case ">":
-                dp++;
-                break;
-            case "<":
-                dp--;
-                break;
-            case ".":
-                rl.write(String.fromCharCode(data[dp]));
-                break;
-            case ",":
-                let res = "";
-                do {
-                    res = await rl.question(": ");
-                } while (res.length === 0);
-                data[dp] = res.charCodeAt(0);
-                break;
-            case "[":
-                sBrackets.push(i);
-                if (data[dp] !== 0) break;
-                for (let j = i + 1; j < line.length; j++) {
-                    if (line[j] === "[") {
-                        sBrackets.push(j);
-                    } else if (line[j] === "]") {
-                        eBrackets.push(j);
-                    }
-                    if (sBrackets.length === eBrackets.length) break;
+
+for (let i = 0; i < code.length; i++) {
+    switch (code[i]) {
+        case "+":
+            data[dp]++;
+            break;
+        case "-":
+            data[dp]--;
+            break;
+        case ">":
+            dp++;
+            break;
+        case "<":
+            dp--;
+            break;
+        case ".":
+            rl.write(String.fromCharCode(data[dp]));
+            break;
+        case ",":
+            let res = "";
+            do {
+                res = await rl.question(": ");
+            } while (res.length === 0);
+            data[dp] = res.charCodeAt(0);
+            break;
+        case "[":
+            sBrackets.push(i);
+            if (data[dp] !== 0) break;
+
+            for (let j = i + 1; j < code.length; j++) {
+                if (code[j] === "[") {
+                    sBrackets.push(j);
+                } else if (code[j] === "]") {
+                    eBrackets.push(j);
                 }
-                if (sBrackets.length !== eBrackets.length)
-                    throw new Error(
-                        `Could not find ending bracket for starting bracket on ${i}`
-                    );
-                sBrackets.pop();
-                i = /** @type {number} */ (eBrackets.pop());
-                break;
-            case "]":
-                if (sBrackets.length === 0)
-                    throw new Error(
-                        `Could not find starting bracket for end bracket on ${i}`
-                    );
-                const lastBr = /** @type {number} */ (sBrackets.pop());
-                i = lastBr - 1;
-                break;
-            default:
-                break;
-        }
+                if (sBrackets.length === eBrackets.length) break;
+            }
+
+            if (sBrackets.length !== eBrackets.length)
+                throw new Error(
+                    `Could not find ending bracket for starting bracket on ${i}`
+                );
+
+            sBrackets.pop();
+            i = /** @type {number} */ (eBrackets.pop());
+            break;
+        case "]":
+            if (sBrackets.length === 0)
+                throw new Error(
+                    `Could not find starting bracket for end bracket on ${i}`
+                );
+            const lastBr = /** @type {number} */ (sBrackets.pop());
+            i = lastBr - 1;
+            break;
+
+        default:
+            break;
     }
-});
+}
 
 // Cleanup
-fileReader.on("close", () => {
-    rl.close();
-});
+rl.close();
