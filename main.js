c88e43f257f02d8b007eb1ced6f1e9d02067b92c
sutaC <catus.pl@gmail.com>
2024-06-26 15:00:40 +0200

    Basic comands

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 // @ts-check
+import readline from "readline/promises";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const code =
+    "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
 
 const data = /** @type {number[]} */ (new Array(30000)).fill(0);
-const code =
-    "+++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++.<.";
 let dp = 0;
+
+const sBrackets = /** @type {number[]} */ [];
+const eBrackets = /** @type {number[]} */ [];
 
 for (let i = 0; i < code.length; i++) {
     switch (code[i]) {
@@ -20,9 +30,24 @@ for (let i = 0; i < code.length; i++) {
             dp--;
             break;
         case ".":
-            console.log(String.fromCharCode(data[dp]));
+            rl.write(String.fromCharCode(data[dp]));
+            break;
+        case ",":
+            let res = "";
+            do {
+                res = await rl.question(": ");
+            } while (res.length === 0);
+            data[dp] = res.charCodeAt(0);
+            break;
+        case "[":
+            // TODO
+            break;
+        case "]":
+            // TODO
             break;
         default:
             continue;
     }
 }
+
+rl.close();
